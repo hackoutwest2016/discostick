c62ea999088374490087770fb56c9a19af3d07cc
Johan Brook <johan.jvb@gmail.com>
2016-08-10 10:34:05 +0200

    Use image instead of video

--- client/home.js
+++ client/home.js
@@ -24,6 +24,10 @@ Template.home.events({
   }
 });
 
+Template.home.onRendered(function() {
+  $('body').addClass('body-home');
+});
+
 Template.home.helpers({
   games() {
     return Games.find();
@@ -32,4 +36,8 @@ Template.home.helpers({
   joinedUsers() {
     return Meteor.users.find({_id: {$in: this.users}});
   }
+});
+
+Template.home.onDestroyed(function() {
+  $('body').removeClass('body-home');
 });
