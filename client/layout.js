6a4c575ce743b6033a5f0c584690195a2138bd69
Johan Brook <johan.jvb@gmail.com>
2016-08-10 11:49:34 +0200

    Fix layout issues

--- client/layout.js
+++ client/layout.js
@@ -1,4 +1,34 @@
+const getGame = () => {
+	const name = FlowRouter.getParam('name');
+	return Games.findOne({name: name});
+};
+
+const getRound = game => {
+	return _.last(game.rounds);
+};
+
 Template.layout.events({
+  'click [data-leave]': function(evt) {
+		evt.preventDefault();
+
+		const game = getGame();
+
+		if (game) {
+			Games.update(game._id, {
+				$pull: {
+					users: Meteor.userId()
+				}
+			});
+
+			if (Session.get('isHost')) {
+				Games.remove(game._id);
+				Session.set('isHost', false);
+			}
+		}
+
+		FlowRouter.go('home'); // you're drunk
+	},
+
   'click [data-new-game]': function(evt) {
     evt.preventDefault();
 
@@ -26,8 +56,8 @@ Template.layout.events({
 });
 
 Template.layout.helpers({
-  started() {
-    return Meteor.userId() || Session.get('isHost');
+  inGame() {
+    return FlowRouter.getParam('name');
   },
 
   isHost() {
